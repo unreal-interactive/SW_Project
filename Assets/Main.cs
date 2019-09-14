46cee262a8692d22f551d30836fdad7d84d29b46
Koous61 <dima_nichiporchik@mail.ru>
2019-09-14 16:13:24 +0300

    edit scene

---
+++ Assets/Main.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public Light lightOne;
+
+    public Light lightTwo;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+}
